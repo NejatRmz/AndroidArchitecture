c0bdc7d25d3920b74703339555d0e468f99204df
Nejat <nejatramiz@gmail.com>
2022-02-02 23:56:30 +0300

    ViewModel class implemented and live data dependency added

--- app/src/main/java/com/example/androidarchitecture/mvvm/CountriesViewModel.java
+++ app/src/main/java/com/example/androidarchitecture/mvvm/CountriesViewModel.java
@@ -2,6 +2,10 @@ package com.example.androidarchitecture.mvvm;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
 import com.example.androidarchitecture.model.DataService;
 import com.example.androidarchitecture.model.Model;
 import com.example.androidarchitecture.mvc.MVCActivity;
@@ -18,16 +22,24 @@ import retrofit2.Response;
  * @author n84190005
  * @since 2022-02-02
  */
-public class MVVMController {
+public class CountriesViewModel extends ViewModel {
     private static final String TAG = "MVVMCONTROLLER";
 
-    private MVCActivity view;
+    private final MutableLiveData<List<Model>> countries = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> countryErrors = new MutableLiveData<>();
     private DataService service;
 
-    public MVVMController(MVCActivity view){
-        this.view = view;
+    public CountriesViewModel(){
         service = new DataService();
         fetchData();
+    }
+
+    public LiveData<List<Model>> getCountries(){
+        return countries;
+    }
+
+    public LiveData<Boolean> getCountryErrors(){
+        return countryErrors;
     }
 
     private void fetchData() {
@@ -38,17 +50,16 @@ public class MVVMController {
                 if (response.isSuccessful()) {
                     Log.d(TAG, "Success");
 
-                    view.setValues(response.body());
+                    countries.setValue(response.body());
+                    countryErrors.setValue(false);
                 }
             }
 
             @Override
             public void onFailure(Call<List<Model>> call, Throwable t) {
                 Log.e(TAG, "Failure: " + t.getMessage());
+                countryErrors.setValue(true);
             }
-
         });
-
-
     }
 }
